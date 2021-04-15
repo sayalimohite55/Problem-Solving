634330cf6e199feb4c0196965aeaaf5f3b975cb6
sayali.mohite <sayali.mohite@springct.com>
2021-04-15 13:28:06 +0530

    * Handled few more scenario's in LinkedList such as : * Remove duplicates from a linked list * Print Kth to the last element from linked list * Delete the middlemost element from the linked list when size of linked list is not known

--- Java/LinkedLists/SinglyLinkedList.java
+++ Java/LinkedLists/SinglyLinkedList.java
@@ -1,8 +1,16 @@
 /**
 * Question: Write a program to implement singly linked list in java without using any build-in data structure
+* Usecases covered :
+			1. Create, insert, delete from linkedlist and print linkedlist
+			2. Remove Duplicates
+			3. Return Kth element to the last element
+			4. Delete the middle node
+			5. Loop Detection
 */
 
 import java.util.Scanner;
+import java.util.HashSet;
+
 
 class Node {
 	int data;
@@ -51,11 +59,6 @@ class SinglyLinkedList {
 		}
 	}
 	
-	void menu() {
-		System.out.print("\n1.Add element\n2.Remove last element\n3.Print Linked List\n4.Delete given data node"
-						+"\n0.Exit\nEnter your choice : ");
-	}
-	
 	private void printList() {
 		if(root == null)
 			System.out.println("\nList is empty");
@@ -97,6 +100,85 @@ class SinglyLinkedList {
 		}
 	}
 	
+	void menu() {
+		System.out.print("\n\n\n1.Add element\n2.Remove last element\n3.Print Linked List\n4.Delete given data node"
+						+"\n5.Remove Duplicates\n6.Print Kth to the Last Element\n7.Delete the middle Node"
+						+"\n0.Exit\nEnter your choice : ");
+	}
+	
+	private void removeDuplicates() {
+		if(root == null)	System.out.println("\nList is empty");
+		else {
+			HashSet<Integer> set = new HashSet<>();
+			Node temp = root, prev = root;
+			
+			while(temp != null) {
+				if(set.contains(temp.data))
+				{
+					prev.next = temp.next;
+					temp = prev.next;
+				}
+				else {
+					set.add(temp.data);
+					prev=temp;
+					temp = temp.next;
+				}
+			}
+			printList();
+		}
+	}
+	
+	private void kthToLastElement(int k) {
+		if(root == null)
+			System.out.println("\nList is empty");
+		else
+		{
+			int n =0;
+			Node temp = root;
+			while(temp != null) {
+				n++;
+				temp = temp.next;
+			}
+			
+			if(n<k) System.out.println("\nInvalid value of k");
+			else if(n==k) System.out.println("\nKth to last element is: "+root.data);
+			else {
+				temp = root;
+				for(int i=1; i<n-k ; i++, temp = temp.next);
+				System.out.println("\nKth to last element is: "+temp.data);
+			}
+		}
+	}
+	
+	private void deleteMiddleNode() {
+		if(root == null)
+			System.out.println("\nList is empty");
+		else
+		{
+			Node p1 = root, p2 = root;
+			while(p2 != null && p2.next != null) {
+				p1 = p1.next;
+				p2 = p2.next.next;
+			}
+			
+			p2 = p1.next;
+			
+			int deleteNode = p1.data;
+			
+			//swap data to delete node
+			p1.data = p1.data + p2.data;
+			p2.data = p1.data - p2.data;
+			p1.data = p1.data - p2.data;
+			
+			//now delete p2
+			p1.next = p2.next;
+			p2 = null;
+			
+			System.out.println("\nMiddle node is "+deleteNode+" and it is deleted");
+			printList();
+		}
+	}
+	
 	public static void main(String args[]) {
 		SinglyLinkedList list = new SinglyLinkedList();
 		Scanner scanner = new Scanner(System.in);
@@ -114,16 +196,12 @@ class SinglyLinkedList {
 				}
 				
 			case 2: // Remove last element
-				{
-					list.removeLast();
-					break;
-				}
-			
+				list.removeLast();
+				break;
+						
 			case 3: //Print List
-				{
 					list.printList();
 					break;
-				}
 			
 			case 4: //Delete given data node
 				{
@@ -132,6 +210,22 @@ class SinglyLinkedList {
 					list.deleteNode(data);
 					break;
 				}
+			
+			case 5: //Remove Duplicates
+				list.removeDuplicates();
+				break;
+				
+			case 6: //Print Kth to the Last Element
+				{
+					System.out.print("\nEnter value of K: ");
+					int k = scanner.nextInt();
+					list.kthToLastElement(k);
+					break;
+				}
+				
+			case 7: //Delete the middle Node"
+				list.deleteMiddleNode();
+				break;
 				
 			case 0: //Exit
 				break;
